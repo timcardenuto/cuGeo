5f7654600c31f7400de76002876ee3e4fe7caf33
tcarden1 <tcarden1@jhu.edu>
2016-10-13 00:40:29 -0400

    updated test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./cuGEO --verbose
+./cuGEO --smem --verbose
 
 echo -n "Test 1... "
 returncode=1
@@ -28,7 +28,43 @@ if [ "$line" = "Done" ]
 then
   returncode=0
 fi
-done < <(./cuGEO --measurements 1000)
+done < <(./cuGEO --measurements 1024 --verbose)
+
+if [ "$returncode" = "0" ]
+then
+  echo Succeeded
+else
+  echo Failed
+  exit $returncode
+fi
+
+echo -n "Test 3... "
+returncode=1
+while read -r line
+do
+if [ "$line" = "Done" ]
+then
+  returncode=0
+fi
+done < <(./cuGEO --iterations 1000 --measurements 1024 --blocks 32 --threads 1024)
+
+if [ "$returncode" = "0" ]
+then
+  echo Succeeded
+else
+  echo Failed
+  exit $returncode
+fi
+
+echo -n "Test 4... "
+returncode=1
+while read -r line
+do
+if [ "$line" = "Done" ]
+then
+  returncode=0
+fi
+done < <(./cuGEO --measurements 1024 --smem --verbose)
 
 if [ "$returncode" = "0" ]
 then
